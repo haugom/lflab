3ba10a70d954fe79997915b0e918ccf458c0b808
Geir Haugom <haugom@gmail.com>
2022-11-10 16:26:05 +0000

    task: Testing kube-apiserver <> etcd encryption

---
+++ kubeyaml/05_etcd/e_alias.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+alias e='ETCDCTL_API=3 \
+ETCDCTL_CACERT=/etc/kubernetes/pki/etcd/ca.crt \
+ETCDCTL_CERT=/etc/kubernetes/pki/etcd/server.crt \
+ETCDCTL_KEY=/etc/kubernetes/pki/etcd/server.key \
+etcdctl --endpoints=https://127.0.0.1:2379'
+
